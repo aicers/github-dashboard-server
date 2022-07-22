d460fb0852e8825d8674f83aa0e19f238559660d
Hanbeom kim <hbkim@einsis.com>
2022-07-22 15:14:03 +0900

    Add User_Agent Header
    - Fix send_github_query function,Config struct
    - Add README.md contents(about token)
    Resolves: #26

--- src/github.rs
+++ src/github.rs
@@ -11,18 +11,26 @@ use reqwest::Client;
 pub struct OpenIssues;
 
 const GITHUB_URL: &str = "https://api.github.com/graphql";
+const APP_USER_AGENT: &str = concat!(env!("CARGO_PKG_NAME"), "/", env!("CARGO_PKG_VERSION"),);
 
 pub async fn send_github_query(
     owner: &str,
     name: &str,
+    token: &str,
 ) -> Result<Response<open_issues::ResponseData>> {
     let variables = open_issues::Variables {
         owner: owner.to_string(),
         name: name.to_string(),
     };
     let request_body = OpenIssues::build_query(variables);
-    let client = Client::new();
-    let res = client.post(GITHUB_URL).json(&request_body).send().await?;
+    let client = Client::builder().user_agent(APP_USER_AGENT).build()?;
+    let res = client
+        .post(GITHUB_URL)
+        .bearer_auth(token)
+        .json(&request_body)
+        .send()
+        .await?;
     let response_body: Response<open_issues::ResponseData> = res.json().await?;
+    println!("{:#?}", response_body);
     Ok(response_body)
 }
