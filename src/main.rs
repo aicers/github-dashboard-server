72eba4a12e0f36ac72f0754e0e2621f079b6d0ee
Soo Lee <soojlee0106@einsis.com>
2022-07-25 13:14:06 +0900

    Apply SRP
    Fixes: #25

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,7 @@ mod github;
 mod graphql;
 mod web;
 
-use conf::{load_info, parse_socket_addr};
+use conf::{load_config, parse_socket_addr};
 use github::send_github_query;
 use std::env;
 use std::process::exit;
@@ -12,15 +12,29 @@ use std::process::exit;
 async fn main() {
     println!("AICE GitHub Dashboard Server");
 
-    let args = env::args();
+    let mut usage_args = env::args();
 
-    let config = match load_info(args) {
+    let path = if let Some(args_val) = usage_args.nth(1) {
+        match args_val.as_str() {
+            "-V" | "--version" => {
+                println!("{} {}", conf::PKG_NAME, conf::PKG_VER);
+                exit(0);
+            }
+            "-h" | "--help" => {
+                println!("{}", conf::USG);
+                exit(0);
+            }
+            _ => args_val,
+        }
+    } else {
+        eprintln!("No file name given. Refer to usage. \n{}", conf::USG);
+        exit(1);
+    };
+
+    let config = match load_config(path.as_ref()) {
         Ok(ret) => ret,
-        Err(_error) => {
-            eprintln!(
-                "Problem while loading info. Refer to usage below. \n{}",
-                conf::USG
-            );
+        Err(error) => {
+            eprintln!("Problem while loading config. {}", error);
             exit(1);
         }
     };
