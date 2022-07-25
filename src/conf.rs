34d1a8e1462ed45d3455eff4fd1b15f1775bd477
Soo Lee <soojlee0106@einsis.com>
2022-07-25 13:14:06 +0900

    Apply SRP
    Fixes: #25

--- src/conf.rs
+++ src/conf.rs
@@ -1,11 +1,10 @@
 use anyhow::{anyhow, bail, Result};
 use serde::Deserialize;
 use std::env;
-use std::env::Args;
 use std::fs::File;
 use std::io::prelude::*;
 use std::net::{SocketAddr, ToSocketAddrs};
-use std::process::exit;
+use std::path::Path;
 
 #[derive(Debug, Deserialize)]
 pub struct ServerAddr {
@@ -30,8 +29,8 @@ pub struct Config {
     pub certification: Certification,
 }
 
-const PKG_NAME: &str = env!("CARGO_PKG_NAME");
-const PKG_VER: &str = env!("CARGO_PKG_VERSION");
+pub const PKG_NAME: &str = env!("CARGO_PKG_NAME");
+pub const PKG_VER: &str = env!("CARGO_PKG_VERSION");
 pub const USG: &str = "USAGE:
     github-dashboard-server <CONFIG>
 
@@ -42,45 +41,22 @@ FLAGS:
 ARG:
     <CONFIG>    A TOML config file";
 
-pub fn load_info(mut args: Args) -> Result<Config> {
+pub fn load_config(path: &Path) -> Result<Config> {
     let mut config_str = String::new();
 
-    if let Some(args_val) = args.nth(1) {
-        match args_val.as_str() {
-            "-V" | "--version" => {
-                println!("{} {}", PKG_NAME, PKG_VER);
-                exit(0);
-            }
-            "-h" | "--help" => {
-                println!("{}", USG);
-                exit(0);
-            }
-
-            default => {
-                if default.contains(".toml") {
-                    if let Err(e) =
-                        File::open(default).and_then(|mut f| f.read_to_string(&mut config_str))
-                    {
-                        bail!("Failed to open file, Please check file name: {:?}", e);
-                    }
-                    let config = match toml::from_str::<Config>(&config_str) {
-                        Ok(ret) => ret,
-                        Err(e) => {
-                            bail!(
-                                "Failed to parse Toml document, Please check file contents: {:?}",
-                                e
-                            );
-                        }
-                    };
-                    Ok(config)
-                } else {
-                    bail!("Failed to load args, Please enter correct args value");
-                }
-            }
-        }
-    } else {
-        bail!("Failed to load args, Please enter the args and run it");
+    if let Err(e) = File::open(path).and_then(|mut f| f.read_to_string(&mut config_str)) {
+        bail!("Failed to open file, Please check file name: {:?}", e);
     }
+    let config = match toml::from_str::<Config>(&config_str) {
+        Ok(ret) => ret,
+        Err(e) => {
+            bail!(
+                "Failed to parse Toml document, Please check file contents: {:?}",
+                e
+            );
+        }
+    };
+    Ok(config)
 }
 
 pub fn parse_socket_addr(addr_str: &str) -> Result<SocketAddr> {
