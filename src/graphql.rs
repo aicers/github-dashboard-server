6b234153dcd3486872b9db372ab7aecd6482ae4c
Hanbeom kim <hbkim@einsis.com>
2022-08-01 11:05:25 +0900

    Return issues value
    Resolves:#37

--- src/graphql.rs
+++ src/graphql.rs
@@ -1,22 +1,29 @@
-use async_graphql::{EmptyMutation, EmptySubscription, Object, Schema, SimpleObject};
+use crate::database::Database;
+use anyhow::{anyhow, Result};
+use async_graphql::{Context, EmptyMutation, EmptySubscription, Object, Schema, SimpleObject};
 
 pub struct Query;
 
 #[derive(SimpleObject)]
 pub struct Issue {
-    owner: String,
-    repo: String,
-    number: i32,
-    title: String,
+    pub owner: String,
+    pub repo: String,
+    pub number: i32,
+    pub title: String,
 }
 
 #[Object]
 impl Query {
-    pub async fn issues(&self) -> Vec<Issue> {
-        Vec::new()
+    pub async fn issues<'ctx>(&self, ctx: &Context<'ctx>) -> Result<Vec<Issue>> {
+        match ctx.data::<Database>() {
+            Ok(db_conn_pool) => Ok(db_conn_pool.select_all()?),
+            Err(error) => Err(anyhow!("{:?}", error)),
+        }
     }
 }
 
-pub fn schema() -> Schema<Query, EmptyMutation, EmptySubscription> {
-    Schema::new(Query, EmptyMutation, EmptySubscription)
+pub fn schema(database: Database) -> Schema<Query, EmptyMutation, EmptySubscription> {
+    Schema::build(Query, EmptyMutation, EmptySubscription)
+        .data(database)
+        .finish()
 }
